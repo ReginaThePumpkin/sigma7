6f87d176fe23c7c68f3b43d5f7b9329104140a56
kevin <kevin.mouse@hotmail.com>
2019-07-05 23:39:54 -0500

    Esto es un nuevo commit cuyo titulo difiere de la rama Kevin

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 5.5 Version 2 COn algo nuevo
+                    Laravel 5.5 Titulo del nuevo commit de Master
                 </div>
 
                 <div class="links">
